20e944602d7a22602b8266ffc42d6c3d7b91e75b
Tosin Omotayo <oluwaetosin@gmail.com>
2022-03-07 07:15:02 +0100

    Add graphql

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./contants";
-import { Post } from "./entities/Post";
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql';
+
 import mikroConfig from './mikro-orm.config'
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async ()=> {
+    
     const orm = await MikroORM.init(mikroConfig);
     orm.getMigrator().up();
-  const post =   orm.em.create(Post, {title: 'My First Post', createdAt: new Date(), updatedAt: new Date()});
-  await orm.em.persistAndFlush(post);
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+
+        })
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, ()=>{
+        console.log("server started on port 4000");
+    });
+    // const post =   orm.em.create(Post, {title: 'My First Post', createdAt: new Date(), updatedAt: new Date()});
+    // await orm.em.persistAndFlush(post);
 } 
 
 main().catch((err)=>console.log(err));
